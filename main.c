2da9dff687cb84b4c87cf4a7599565956af3dec1
yehusheng <yehusheng@foxmail.com>
2015-02-11 13:45:32 +0800

    add main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  helloworld
+ *
+ *        Version:  1.0
+ *        Created:  2015年02月11日 13时37分24秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YeHaiSheng (), yehusheng@foxmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+	printf("hello world !\n");
+	return 0;
+}
